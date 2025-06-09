b2ceb5d4d8834c70584dd0466262fa9269cda001
Doniyor <doniyork334@gmail.com>
2025-06-09 19:37:46 +0500

    Testing connection method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int result = subtract(10, 5);
+        System.out.println("Here's the result => " + result);
+    }
+    public static int subtract(int a, int b) {
+        return a - b;
     }
 }
