ea9ce68682a29ff9db0fa9b793ec18d4743d5e75
Sadriddin Sultanbayevs <sultanbayevsae1@gmail.com>
2025-06-11 16:08:54 +0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,5 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        int result = subtract(10, 5);
-        System.out.println("Here's the result => " + result);
-    }
-    public static int subtract(int a, int b) {
-        return a - b;
-    }
+   
 }
