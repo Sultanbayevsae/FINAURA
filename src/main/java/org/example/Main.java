d09eb34381d28a6febf995e9c3be7de89607e336
DoniyorKhasanov1 <doniyork334@gmail.com>
2025-06-13 15:42:57 +0500

    Configuration

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ import org.hibernate.cfg.Configuration;;
 public class Main {
     public static void main(String[] args) {
 
-                SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+                SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
 
                 Session session = sessionFactory.openSession();
                 session.beginTransaction();
